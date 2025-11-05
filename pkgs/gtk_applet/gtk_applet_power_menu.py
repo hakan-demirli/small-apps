5ffed994e3a2659d079a4458b4281254900ea789
hakan-demirli <ehdemirli@gmail.com>
2025-11-05 23:31:55 +0300

    refactor(power-menu): move action handling to subprocess

--- pkgs/gtk_applet/gtk_applet_power_menu.py
+++ pkgs/gtk_applet/gtk_applet_power_menu.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 # ruff: noqa E402
 
+import sys
 import signal
 import subprocess
+import argparse
 
 import gi
 
@@ -22,41 +24,15 @@ class AppIndicatorExample:
         )
         self.indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
         self.indicator.set_menu(self.create_menu())
-        self.indicator.set_label("⏻", self.app)  # Set power symbol here
+        self.indicator.set_label("⏻", self.app)
 
     def create_menu(self):
         menu = Gtk.Menu()
-
-        self.add_menu_item(
-            menu,
-            "Sleep",
-            self.confirm_action,
-            "Sleep",
-            "Are you sure you want to sleep?",
-        )
-        self.add_menu_item(
-            menu,
-            "Reboot",
-            self.confirm_action,
-            "Reboot",
-            "Are you sure you want to reboot?",
-        )
-        self.add_menu_item(
-            menu,
-            "Shutdown",
-            self.confirm_action,
-            "Shutdown",
-            "Are you sure you want to shut down?",
-        )
-        self.add_menu_item(
-            menu,
-            "Logout",
-            self.confirm_action,
-            "Logout",
-            "Are you sure you want to logout?",
-        )
-        # self.add_menu_item(menu, "Quit", self.quit)
-
+        self.add_menu_item(menu, "Sleep", self.on_menu_item_click, "sleep")
+        self.add_menu_item(menu, "Hibernate", self.on_menu_item_click, "hibernate")
+        self.add_menu_item(menu, "Reboot", self.on_menu_item_click, "reboot")
+        self.add_menu_item(menu, "Shutdown", self.on_menu_item_click, "shutdown")
+        self.add_menu_item(menu, "Logout", self.on_menu_item_click, "logout")
         menu.show_all()
         return menu
 
@@ -65,53 +41,79 @@ class AppIndicatorExample:
         item.connect("activate", callback, *args)
         menu.append(item)
 
-    def confirm_action(self, source, action_name, confirmation_text):
-        dialog = Gtk.MessageDialog(
-            parent=None,
-            flags=0,
-            message_type=Gtk.MessageType.QUESTION,
-            buttons=Gtk.ButtonsType.YES_NO,
-            text=f"Confirm {action_name}",
-        )
-        dialog.format_secondary_text(confirmation_text)
+    def on_menu_item_click(self, source, action_name):
+        script_path = sys.argv[0]
+        subprocess.Popen([script_path, "--action", action_name])
 
-        settings = Gtk.Settings.get_default()
-        settings.props.gtk_application_prefer_dark_theme = True
+    def run(self):
+        Gtk.main()
 
-        response = dialog.run()
-        dialog.destroy()
 
-        if response == Gtk.ResponseType.YES:
-            if action_name == "Sleep":
-                self.sleep()
-            elif action_name == "Shutdown":
-                self.shutdown()
-            elif action_name == "Reboot":
-                self.reboot()
-            elif action_name == "Logout":
-                self.logout()
+def show_confirmation_dialog(action_name, confirmation_text, command):
+    Gtk.init(None)
+    dialog = Gtk.MessageDialog(
+        parent=None,
+        flags=0,
+        message_type=Gtk.MessageType.QUESTION,
+        buttons=Gtk.ButtonsType.YES_NO,
+        text=f"Confirm {action_name.capitalize()}",
+    )
+    dialog.format_secondary_text(confirmation_text)
 
-    def reboot(self):
-        subprocess.run(["systemctl", "reboot"])
+    settings = Gtk.Settings.get_default()
+    settings.props.gtk_application_prefer_dark_theme = True
 
-    def shutdown(self):
-        subprocess.run(["systemctl", "poweroff"])
+    response = dialog.run()
+    dialog.destroy()
 
-    def sleep(self):
-        subprocess.run(["systemctl", "suspend"])
-
-    def logout(self):
-        subprocess.run(["hyprctl", "dispatch", "exit"])
-
-    def quit(self, source):
-        Gtk.main_quit()
+    if response == Gtk.ResponseType.YES:
+        subprocess.run(command)
 
 
 def main():
-    Gtk.init(None)
-    AppIndicatorExample()
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
-    Gtk.main()
+    actions = {
+        "sleep": {
+            "text": "Are you sure you want to sleep?",
+            "command": ["systemctl", "suspend"],
+        },
+        "hibernate": {
+            "text": "Are you sure you want to hibernate?",
+            "command": ["systemctl", "hibernate"],
+        },
+        "reboot": {
+            "text": "Are you sure you want to reboot?",
+            "command": ["systemctl", "reboot"],
+        },
+        "shutdown": {
+            "text": "Are you sure you want to shut down?",
+            "command": ["systemctl", "poweroff"],
+        },
+        "logout": {
+            "text": "Are you sure you want to logout?",
+            "command": ["hyprctl", "dispatch", "exit"],
+        },
+    }
+
+    if len(sys.argv) > 1:
+        parser = argparse.ArgumentParser(description="Power menu confirmation tool.")
+        parser.add_argument(
+            "--action",
+            type=str,
+            required=True,
+            choices=actions.keys(),
+            help="The power action to perform.",
+        )
+        args = parser.parse_args()
+
+        action_details = actions.get(args.action)
+        if action_details:
+            show_confirmation_dialog(
+                args.action, action_details["text"], action_details["command"]
+            )
+    else:
+        app = AppIndicatorExample()
+        signal.signal(signal.SIGINT, signal.SIG_DFL)
+        app.run()
 
 
 if __name__ == "__main__":
